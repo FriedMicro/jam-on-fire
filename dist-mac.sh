9ff2d2d61730350bb84fe62bbf123efce9a383f4
Lucas Smith <friedmicrochips@gmail.com>
2018-01-28 15:07:15 -0500

    Update build script

--- dist-mac.sh
+++ dist-mac.sh
@@ -1,3 +1,3 @@
 python2 -m pip install cx_Freeze --upgrade
-cxfreeze init.py --target-dir dist --include-modules=pygame --icon=./res/BigPurple_Speaker.png.icns
+cxfreeze game.py --target-dir dist --include-modules=pygame --icon=./res/BigPurple_Speaker.png.icns
 cp -R res dist/
