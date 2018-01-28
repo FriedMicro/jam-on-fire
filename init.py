07acdb95db6d252c2bb451fcd152e83ba6980b25
Allie Curtis <accurtis@bsu.edu>
2018-01-28 10:06:52 -0500

    Overhaul boilerplate
    I'm so sorry I'll do smaller-sized version control changes now

--- init.py
+++ init.py
@@ -1,18 +1,104 @@
-import pygame
+import sys, pygame
 from pygame.locals import *
 import os
 
 pygame.init()
 pygame.font.init()
+pygame.event.pump()
 
+class Button:
+
+    def __init__(self, imageName, pos = (0,0), index = None):
+        self.image = pygame.image.load(imageName)
+        self.rect = self.image.get_rect()
+        self.pos = pos
+        self.index = index
+        if index is not None:
+            self.x = posX[index]
+            self.y = posY[index]
+            self.pos = (self.x, self.y)
+        self.rect.center = self.pos
+
+    def move(self, pos):
+        self.pos = pos
+        self.x = pos[0]
+        self.y = pos[1]
+        self.rect.center = self.pos
+
+    def move_by_index(self):
+        if self.index == 0:
+            self.move((posX[1], self.y))
+            self.index = 1
+        elif self.index == 1:
+            self.move((posX[2], self.y))
+            self.index = 2
+        else:
+            self.move((posX[0], self.y))
+            self.index = 0
+
+def render_empty_buttons():
+    for i in posX:
+        for j in posY:
+            emptyButton.move((i,j))
+            screen.blit(emptyButton.image, emptyButton.rect)
+
+def render_buttons():
+    render_empty_buttons()
+    for button in buttons:
+        screen.blit(button.image, button.rect)
+
+def move_button(button):
+    screen.fill((0,0,0), button.rect)
+    screen.blit(emptyButton.image, button.rect)
+    pygame.display.update(button.rect)
+
+    button.move_by_index()
+    screen.blit(button.image, button.rect)
+    pygame.display.update(button.rect)
+
+def button_event(button):
+    if button == powerButton:
+        pygame.mixer.stop()
+        pygame.display.quit()
+        sys.exit()
+    elif button in gameButtons:
+        move_button(button)
+    elif button == speakerButton:
+        introAudio.stop()
+
+def mouse_click(pos):
+    for button in buttons:
+        if button.rect.collidepoint(pos):
+            button_event(button)
+
+def event_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == 5:
+                mouse_click(pygame.mouse.get_pos())
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 size = width, height = 320, 240
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size, pygame.NOFRAME)
+screen.fill( (0,0,0) ) #black
 
-#Loosely based off: https://github.com/jpritcha3-14/shooting-game/blob/master/shooting_game.py
-def render_image(name, x, y):
-    filepath = os.path.join('/images', name)
-    try:
-        image = pygame.image.load(filepath)
-    except:
-        print('Cannot load', name)
-    image = image.convert()
+posX = [width*0.25, width*0.5, width*0.75]
+posY = [height*0.3, height*0.5, height*0.7]
+emptyButton = Button("res/Empty_Button.png")
+powerButton = Button("res/Power_Button.png", (width-32,32))
+speakerButton = Button("res/Speaker_Button.png", (32, 32))
+purpleButton = Button("res/Purple_Button.png", None, 0)
+pinkButton = Button("res/Pink_Button.png", None, 1)
+cyanButton = Button("res/Cyan_Button.png", None, 2)
+gameButtons = [purpleButton, pinkButton, cyanButton]
+buttons = gameButtons + [powerButton, speakerButton]
+render_buttons()
+
+introAudio = pygame.mixer.Sound("res/Intro.wav")
+
+pygame.display.update()
+
+introAudio.set_volume(0.3)
+introAudio.play(-1, 0, 2000)
+
+event_loop()
