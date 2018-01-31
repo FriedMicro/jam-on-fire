c83df481ef897c7e4365ff06d8e91d1860848f13
Allie Curtis <accurtis@bsu.edu>
2018-01-31 04:14:25 -0500

    Add documentation
    Couldn't be bothered with it at go time lol

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ pygame.mixer.init()
 pygame.font.init()
 pygame.event.pump()
 
-
+#Ensures random orientation of the one original and two modified audio pieces
 first = random.randint(0,2)
 mid = (1 if (first == 0) else 0)
 if (mid == 0):
@@ -14,8 +14,11 @@ if (mid == 0):
 else:
     last = (2 if (mid == 1) else 1)
 
+#Really should have been called sprites but whatever, my b
 class Button:
 
+    #Index is for the three game buttons - got tired of messing with intricate
+    #positional junk so just labeling them 0, 1, and 2 seemed better (at the time)
     def __init__(self, imageName, pos = (0,0), index = None):
         self.image = pygame.image.load(imageName)
         self.rect = self.image.get_rect()
@@ -34,6 +37,7 @@ class Button:
         self.y = pos[1]
         self.rect.center = self.pos
 
+    #Same line of thinking as the above - easier to position them using indices
     def move_by_index(self):
         if self.index == 0:
             self.move((posX[1], self.y))
@@ -45,10 +49,14 @@ class Button:
             self.move((posX[0], self.y))
             self.index = 0
 
+#I set the background volume way too low (5% of original) but whatevs
 def play_intro():
     intro.set_volume(0.05)
     return intro.play(-1, 0, 800)
 
+#Thought it'd be easier to take one emptyButton object, move it to each of the 9 positions,
+#and write it to the screen (underneath the game buttons) than make 9 emptyButtons
+#since they don't have any properties other than existing
 def render_empty_buttons():
     for i in posX:
         for j in posY:
@@ -66,6 +74,8 @@ def render_buttons():
     for button in gameButtons:
         screen.blit(button.image, button.rect)
 
+#Because the empty buttons are smaller than the game buttons, the rect objects
+#had to be filled in before replacing it with the emptyButton image
 def move_button(button):
     screen.fill((0,0,0), button.rect)
     screen.blit(emptyButton.image, button.rect)
@@ -79,6 +89,8 @@ def switch_speaker(current, change):
     screen.blit(change.image, current.rect)
     pygame.display.update(current.rect)
 
+#For the goal audio, this pauses background music and plays the unaltered pieces at once.
+#Lucas was super happy that we could do this instead of having to combine them on the deck lol
 def play_original():
     channel.pause()
     vocal1.play()
@@ -91,6 +103,8 @@ def stop_original():
     instrument1.stop()
     channel.unpause()
 
+#Gets user's button positions and plays the corresponding audio pieces (based on random indices chosen
+#in lines 10-15)
 def play_combined(pos1, pos2, pos3):
     channel.pause()
     vocals[pos1].play()
@@ -103,6 +117,13 @@ def stop_combined(pos1, pos2, pos3):
     instruments[pos3].stop()
     channel.unpause()
 
+#I hate the way I did this because it doesn't account for the player clicking the speaker,
+#listening to the partial piece, and then moving the button (expecting to hear a change).
+#Pretty sure it would just keep playing the initial piece until the player clicked the
+#speaker again (twice, to mute and play) where it would then register the change...grrr
+
+#Then again...not like we had much time to playtest lol. Also, indices are 0 2 and 4 because
+#only the speakerButtons are used for events (the mute ones are just there for the image)
 def play_piece(index, pos):
     channel.pause()
     if index == 0:
@@ -144,6 +165,11 @@ def get_button_pos(index):
         elif cyanButton.rect.collidepoint( (posX[2], posY[2]) ):
             return last
 
+#There are DEFINITELY so many better ways of doing this, but time was not on my side lol.
+#The mute condition got kind of confusing but its coming from this idea: if speaker.mute
+#is False, the speaker's IMAGE is NOT the mute image. There were no winners here, because my
+#other option was basing it off of audio (mute = False meaning the audio was not playing).
+#However, I included it so I could reference what picture to show, so I did this
 def button_event(button):
     if button == powerButton:
         pygame.mixer.stop()
@@ -208,6 +234,7 @@ posX = [width*0.25+25, width*0.5+10, width*0.75-5]
 posY = [height*0.3+20, height*0.5+20, height*0.7+20]
 
 cwd = os.path.abspath('res/')
+#The bg image and below two lines were literally created in the last 30 minutes
 background = pygame.image.load(cwd + "/Background.png")
 screen.blit(background, background.get_rect())
 emptyButton = Button(cwd + "/Empty_Button.png")
